d5e41de4d4d5e964188e862fcf30b031e3440972
Aditi Diwan <aditid@vt.edu>
2023-04-17 00:54:34 -0400

    rest api with sample db

--- backend/README.md
+++ backend/README.md
@@ -1,3 +1,7 @@
 # Backend
 
 To run the backend, run `npm run dev` in this folder. This command can be modified in the `package.json` file. 
+
+Used following website(s) as a guide:
+* To first test connection to DB: https://www.mongodb.com/blog/post/quick-start-nodejs-mongodb-how-to-get-connected-to-your-database
+* For MERN stack: https://www.mongodb.com/languages/mern-stack-tutorial
---
+++ backend/db/conn.js
@@ -0,0 +1,26 @@
+const { MongoClient } = require("mongodb");
+const uri = process.env.ATLAS_URI;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+ 
+var _db;
+ 
+module.exports = {
+  connectToServer: function (callback) {
+    client.connect(function (err, db) {
+      // Currently just connecting to a sample db object - will change with actual implementation
+      if (db)
+      {
+        _db = db.db("sample_mflix");
+        console.log("Successfully connected to MongoDB."); 
+      }
+      return callback(err);
+    });
+  },
+ 
+  getDb: function () {
+    return _db;
+  },
+};
