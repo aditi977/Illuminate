bcb498e488eee5d9d95c2eed9a5db934114f96cc
Aditi Diwan <aditid@vt.edu>
2023-04-20 21:27:12 -0400

    added Header content

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,19 @@
+import './Header.css'
+import logo from './logo.png'
+
 export default function Header() {
     return (
-      <>
-        
-      </>
+        <div className='header'>
+            <div className='logo'>
+                <img src={logo} height={'66px'}/>
+            </div>
+            <div className='menu-container'>
+                <ul className='menu'>
+                    <li><a href='#'>Search</a></li>
+                    <li><a href='#'>Login</a></li>
+                    <li><a href='#'>Join for Free</a></li>
+                </ul>
+            </div>
+        </div>
     )
   }
