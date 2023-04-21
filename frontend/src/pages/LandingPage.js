a5bed5bc5e6563c3e6e49b37e103b0292de7e92c
Aditi Diwan <aditid@vt.edu>
2023-04-21 10:47:38 -0400

    finished header and middle content

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -1,10 +1,24 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import './LandingPage.css';
+import largeText from './largeText.png';
+import heroImage from './heroImage.png';
 
 export default function LandingPage() {
     return (
       <>
         <Header/>
+        <div className='main-content'>
+        <div className='main-text-content'>
+        <img src={largeText} width='555px'/>
+        <a href="#" className='cta-btn'>Get Started</a>
+        </div>
+
+        <div className='hero-image'>
+          <img src={heroImage} width='444px'/>
+        </div>
+        </div>
+        
         <Footer/>
       </>
     )
