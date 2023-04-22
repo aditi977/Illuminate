77c5d58fa022d7b1b3c190b908fcb562778e2477
UsemVarshita <varshitausem098@gmail.com>
2023-04-21 23:53:13 -0400

    changes in footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,7 +1,25 @@
+import "../components/Footer.css";
+
 export default function Footer() {
     return (
       <>
-        
-      </>
+      <footer>
+      <section class="footer-main-frame">
+      <div class="address-part">
+          <section class="social-media-icons links">
+            <a href to="/"
+            ><i class="fa-brands fa-facebook"></i></a>
+            <a href="/"><i class="fa-brands fa-twitter"></i></a>
+            <a href>
+              <i class="fa-brands fa-linkedin"></i></a>
+            <a href="/">
+              <i class="fa-brands fa-instagram"></i></a>
+          </section>
+        </div>
+        <section class="copy-rights-bar">Copyright ANBS@2023</section>
+       
+
+       
+      </section></footer></>
     )
   }
