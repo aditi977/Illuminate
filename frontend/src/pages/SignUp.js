a0416bc31314a8842b68269f023265bf918dd4ee
UsemVarshita <varshitausem098@gmail.com>
2023-04-23 01:51:53 -0400

    Added Signup form to signup page

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -10,27 +10,27 @@ export default function SignUp() {
          
                     <form>
                         <div class="sign-up-form">
-                            <h1>Sign Up</h1>
-                            <p>Please fill in this form to create an account.</p>
-                            <hr />
+                            <h1>Start your journey today!</h1>
+                           
+                      
 
-                            <label for="email"><b>Email</b></label>
-                            <input type="text" placeholder="Enter Email" name="email" required />
+                           <div class="rectangle1">
+                            <input type="text" placeholder="  Name" name="name" required /></div>
+                            <div class="rectangle2">
+                            <input type="text" placeholder="  Email" name="email" required /></div>
 
-                            <label for="psw"><b>Password</b></label>
-                            <input type="password" placeholder="Enter Password" name="psw" required />
+                          <div class="rectangle3">
+                            <input type="password" placeholder="  New Password" name="psw" required /></div>
 
-                            <label for="psw-repeat"><b>Repeat Password</b></label>
-                            <input type="password" placeholder="Repeat Password" name="psw-repeat" required />
+                            <div class="rectangle4">
+                            <input type="password" placeholder="  Re-enter new Password" name="psw-repeat" required /></div>
 
-                            <label>
-                                <input type="checkbox" checked="checked" name="remember" style={{marginBottom: '15px'}} /> Remember me
-                            </label>
+                           
 
-                            <p>By creating an account you agree to our <a href="#" style={{color:"dodgerblue"}}>Terms & Privacy</a>.</p>
+                            <p>By creating an account you agree to our <a href="#" style={{color:" #FFFFFF"}}>Terms & Privacy</a>.</p>
 
                             <div class="clearfix">
-                                <button type="button" class="cancelbtn">Cancel</button>
+                               
                                 <button type="submit" class="signupbtn">Sign Up</button>
                             </div>
                         </div>
