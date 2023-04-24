af3a366537894f932020181d0649acf32e6ad447
Aditi Diwan <aditid@vt.edu>
2023-04-23 21:08:41 -0400

    fixed sign up form formatting

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -1,42 +1,33 @@
 import Header from "../components/Header";
 import './SignUp.css';
+import arrow from './arrow.png'
 
 export default function SignUp() {
     return (
         <>
             <Header />
             <div className='sign-up'>
-                <div className='top-img'></div>
-         
-                    <form>
-                        <div class="sign-up-form">
-                            <h1>Start your journey today!</h1>
-                           
-                      
 
-                           <div class="rectangle1">
-                            <input type="text" placeholder="  Name" name="name" required /></div>
-                            <div class="rectangle2">
-                            <input type="text" placeholder="  Email" name="email" required /></div>
+                <form>
+                    <div class="sign-up-form">
 
-                          <div class="rectangle3">
-                            <input type="password" placeholder="  New Password" name="psw" required /></div>
+                        <h1><a href='/'><img src={arrow} /></a>&nbsp;&nbsp;Start your journey today!</h1>
 
-                            <div class="rectangle4">
-                            <input type="password" placeholder="  Re-enter new Password" name="psw-repeat" required /></div>
+                        <input type="text" placeholder="  Name" name="name" required />
 
-                           
+                        <input type="text" placeholder="  Email" name="email" required />
 
-                            <p>By creating an account you agree to our <a href="#" style={{color:" #FFFFFF"}}>Terms & Privacy</a>.</p>
+                        <input type="password" placeholder="  New Password" name="psw" required />
 
-                            <div class="clearfix">
-                               
-                                <button type="submit" class="signupbtn">Sign Up</button>
-                            </div>
+                        <input type="password" placeholder="  Re-enter new Password" name="psw-repeat" required />
+
+                        <p>By creating an account you agree to our <a href="#" style={{ color: " #FFFFFF" }}>Terms & Privacy</a>.</p>
+
+                        <div>
+                            <button type="submit" class="signupbtn">Submit</button>
                         </div>
-                    </form>
-         
-                <div className='bottom-img'></div>
+                    </div>
+                </form>
             </div>
         </>
 
