57933c8aa53a8925d09ccdf5d89a9975feae2a9a
UsemVarshita <varshitausem098@gmail.com>
2023-05-02 21:04:09 -0400

    Login page
    Added login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import LandingPage from './pages/LandingPage';
 import { Routes, Route } from "react-router-dom"
 import SignUp from './pages/SignUp';
 import Welcome from './pages/Welcome';
+import Login from './pages/Login';
 import PPForm from './pages/PPForm';
 
 
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={ <LandingPage/> } />
         <Route path="signup" element={ <SignUp/> } />
+        <Route path="login" element={<Login/>}/>
         <Route path="welcome" element={<Welcome isLoggedIn={true}/>} />
         <Route path="form" element={<PPForm/>}/>
       </Routes>
