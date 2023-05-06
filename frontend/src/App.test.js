302d6aa51e63705aa037d11643c23a2cc13bf0ea
UsemVarshita <varshitausem098@gmail.com>
2023-05-06 15:00:07 -0400

    Frontend unit tests
    Added unit tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,84 @@
+
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders join for free link', () => {
+  render(<Header />);
+  const linkElement = screen.getByText(/Join for Free/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders join login link', () => {
+  render(<Header />);
+  const linkElement = screen.getByText(/login/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+test('does not render join login link when logged in', () => {
+  render(<Header isLoggedIn={true} />);
+  const linkElement = screen.queryByText(/login/i);
+  expect(linkElement).not.toBeInTheDocument();
+});
+
+test('renders browse option when logged in', () => {
+  render(<Header isLoggedIn={true} />);
+  const linkElement = screen.queryByText(/Browse/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Personalized Path option when logged in', () => {
+  render(<Header isLoggedIn={true} />);
+  const linkElement = screen.queryByText(/Personalized Path/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Projects option when logged in', () => {
+  render(<Header isLoggedIn={true} />);
+  const linkElement = screen.queryByText(/Projects/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Jobs option when logged in', () => {
+  render(<Header isLoggedIn={true} />);
+  const linkElement = screen.queryByText(/Jobs/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders My Mentor option when logged in', () => {
+  render(<Header isLoggedIn={true} />);
+  const linkElement = screen.queryByText(/My Mentor/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+test('renders Join for Freer option when not logged in', () => {
+  render(<Header isLoggedIn={false} />);
+  const linkElement = screen.queryByText(/Join for Free/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders copyright text', () => {
+  render(<Footer />);
+  const linkElement = screen.getByText(/Copyright @2023 Illuminate, Inc/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('does not renders copyright without year text', () => {
+  render(<Footer />);
+  const linkElement = screen.queryByText(/Copyright@ Illuminate,Inc/i);
+  expect(linkElement).not.toBeInTheDocument();
+});
+
+
+
+
+
+
+
+
+
+
+
