484a9c47b803f8a26391cb42619a0c402428c897
UsemVarshita <varshitausem098@gmail.com>
2023-05-06 15:53:14 -0400

    test cases
    test cases

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,36 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders join for free link', () => {
+  render(<Header />);
+  const linkElement = screen.getByText(/Join for Free/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders join login link', () => {
+  render(<Header />);
+  const linkElement = screen.getByText(/login/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+test('renders copyright text', () => {
+  render(<Footer />);
+  const linkElement = screen.getByText(/Copyright@2023 Illuminate,Inc/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('does not renders copyright without yer text', () => {
+  render(<Footer />);
+  const linkElement = screen.queryByText(/Copyright Illuminate,Inc/i);
+  expect(linkElement).not.toBeInTheDocument();
+});
+
+
+
+
+
+
+
