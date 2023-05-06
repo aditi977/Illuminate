f938ae521c86f29bb0ff80bb082ab591d1f43c27
UsemVarshita <varshitausem098@gmail.com>
2023-05-06 15:52:26 -0400

    test cases
    test cases

---
+++ backend/services/User.test.js
@@ -0,0 +1,99 @@
+
+const User = require('./User');
+const UserModel = require('../models/User');
+const { object, string } = require('yup');
+const bcrypt = require('bcrypt');
+
+  describe('User', () => {
+    describe('signUp', () => {
+      it('should insert a new user into the database', async () => {
+        // Arrange
+        const user = new User({
+          name: 'John Doe',
+          email: 'john@example.com',
+          password: 'password',
+          operation: 'signUp',
+        });
+  
+        // Act
+        const result = await user.perform();
+  
+        // Assert
+        expect(result.id).toBeGreaterThan(0);
+        expect(result.name).toBe(user.name);
+        expect(result.email).toBe(user.email);
+      });
+  
+      it('should throw an error if the email already exists', async () => {
+        // Arrange
+        const existingUser = await UserModel.query().insert({
+          name: 'Jane Doe',
+          email: 'jane@example.com',
+          password: await bcrypt.hash('password', 10),
+        });
+        const user = new User({
+          name: 'John Doe',
+          email: 'jane@example.com',
+          password: 'password',
+          operation: 'signUp',
+        });
+  
+        // Act & Assert
+        await expect(user.perform()).rejects.toThrow('Email already exists.');
+      });
+    });
+  
+    describe('signIn', () => {
+      it('should return a JWT token if the email and password are correct', async () => {
+        // Arrange
+        const existingUser = await UserModel.query().insert({
+          name: 'John Doe',
+          email: 'john@example.com',
+          password: await bcrypt.hash('password', 10),
+        });
+        const user = new User({
+          email: 'john@example.com',
+          password: 'password',
+          operation: 'signIn',
+        });
+  
+        // Act
+        const result = await user.perform();
+  
+        // Assert
+        expect(typeof result).toBe('string');
+        expect(result).not.toBe('');
+      });
+  
+      it('should throw an error if the email does not exist', async () => {
+        // Arrange
+        const user = new User({
+          email: 'john@example.com',
+          password: 'password',
+          operation: 'signIn',
+        });
+  
+        // Act & Assert
+        await expect(user.perform()).rejects.toThrow("Email doesn't exist.");
+      });
+  
+      it('should throw an error if the password is incorrect', async () => {
+        // Arrange
+        const existingUser = await UserModel.query().insert({
+          name: 'John Doe',
+          email: 'john@example.com',
+          password: await bcrypt.hash('password', 10),
+        });
+        const user = new User({
+          email: 'john@example.com',
+          password: 'wrongpassword',
+          operation: 'signIn',
+        });
+  
+        // Act & Assert
+        await expect(user.perform()).rejects.toThrow('Wrong password');
+      });
+    });
+  });
+  
+  
